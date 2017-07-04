c3ff15ee81c59899ea5fc41cd7af6c872c4d596e
Admin <admin@Mac-Admin.local>
2017-07-04 15:26:28 +0300

    Initial Commit

---
+++ EquidistantRectangles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EquidistantRectangles
+//
+//  Created by Admin on 04.07.17.
+//  Copyright © 2017 ilya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
