6f58cd158f6654bd6be57cfe7ee63875f42b0562
Admin <admin@Mac-Admin.local>
2017-07-04 16:21:37 +0300

    Initial commit (crashes on resizing simulator window for some reason)

--- EquidistantRectangles/ViewController.m
+++ EquidistantRectangles/ViewController.m
@@ -7,23 +7,43 @@
 //
 
 #import "ViewController.h"
+#import "Masonry.h"
+
+#define SIDE 30
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSMutableArray<UIView *> *rects;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+-(void) viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+    self.rects = [NSMutableArray new];
+    for(NSUInteger i = 0; i < 4; i++) {
+        UIView *rect = [UIView new];
+        rect.backgroundColor = [UIColor greenColor];
+        [self.rects addObject:rect];
+        [self.view addSubview:rect];
+    }
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    for(NSUInteger i = 0; i < 4; i++) {
+        [self.rects[i] mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.width.equalTo(@SIDE);
+            make.height.equalTo(@SIDE);
+            make.centerY.equalTo(self.view.mas_centerY);
+            NSNumber *leadValue = @((self.view.frame.size.width - 4 * SIDE) * (i + 1) / 5 + i * SIDE);
+            make.leading.equalTo(leadValue);
+        }];
+    }
 }
+
 
 
 @end
